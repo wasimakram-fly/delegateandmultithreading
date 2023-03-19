032703316cb515c62e852c250f7f6d160bb02968
wasimakram-fly <wasimakram8099@gmail.com>
2023-03-19 20:35:50 +0530

    multithreading ex2

---
+++ multithreading_ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new thread and start it
+        Thread t = new Thread(new ThreadStart(DoWork));
+        t.Start();
+
+        // Do some work on the main thread
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Main thread: {0}", i);
+            Thread.Sleep(1000);
+        }
+
+        Console.ReadLine();
+    }
+
+    static void DoWork()
+    {
+        // Do some work on the new thread
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Worker thread: {0}", i);
+            Thread.Sleep(1000);
+        }
+        Console.ReadLine();
+    }
+}
